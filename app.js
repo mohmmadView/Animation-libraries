dee7d28b5e3be0d01d182612a4190b01b361c3a2
darksouls <darksouls@localhost.localdomain>
2022-12-23 21:57:13 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,284 @@
+//TODO -------------------------------------------------------------------------- */
+//?  --------------------------------- moveBorder ------------------------------- */
+//TODO -------------------------------------------------------------------------- */
+let elmTitle = document.querySelector(".title");
+let offsetTitle = anime.setDashoffset(elmTitle);
+elmTitle.setAttribute("stroke-dashoffset", offsetTitle);
+anime({
+  targets: elmTitle,
+  strokeDashoffset: [offsetTitle, 200],
+  duration: anime.random(1000, 3000),
+  delay: anime.random(0, 0),
+  loop: true,
+  direction: "alternate",
+  easing: "easeInOutSine",
+  autoplay: true,
+});
+//TODO -------------------------------------------------------------------------- */
+//?  ------------------------------ TheMoveBorder ------------------------------- */
+//TODO -------------------------------------------------------------------------- */
+//* -------------------------------------------------------------------------- */
+//! ------------------------------ StartAnimate.css ------------------------------ */
+//* -------------------------------------------------------------------------- */
+let elmAnimate = document.querySelector(".layer2");
+let offsetAnimate = anime.setDashoffset(elmAnimate);
+elmAnimate.setAttribute("stroke-dashoffset", offsetAnimate);
+anime({
+  targets: elmAnimate,
+  strokeDashoffset: [offsetAnimate, 25],
+  duration: anime.random(3000, 2000),
+  delay: anime.random(1000, 1000),
+  loop: true,
+  direction: "alternate",
+  easing: "easeInOutSine",
+  autoplay: true,
+});
+//* -------------------------------------------------------------------------- */
+//! ------------------------------ TheEndAnimate.css ------------------------------ */
+//* -------------------------------------------------------------------------- */
+//! -------------------------------------------------------------------------- */
+//* ----------------------------- ClassAnimation.css ----------------------------- */
+//! -------------------------------------------------------------------------- */
+let ArrayListMove = [
+  "animate__bounce",
+  "animate__flash",
+  "animate__pulse",
+  "animate__rubberBand",
+  "animate__shakeX",
+  "animate__shakeY",
+  "animate__headShake",
+  "animate__swing",
+  "animate__tada",
+  "animate__wobble",
+  "animate__jello",
+  "animate__heartBeat",
+  "animate__backInDown",
+  "animate__backInLeft",
+  "animate__backInRight",
+  "animate__backInUp",
+  "animate__backOutDown",
+  "animate__backOutLeft",
+  "animate__backOutRight",
+  "animate__backOutUp",
+  "animate__bounceIn",
+  "animate__bounceInDown",
+  "animate__bounceInLeft",
+  "animate__bounceInRight",
+  "animate__bounceInUp",
+  "animate__fadeIn",
+  "animate__fadeInDownBig",
+  "animate__flipInX",
+  "animate__lightSpeedInLeft",
+  "animate__rotateInDownLeft",
+];
+let elemTextView = document.querySelector(".textView");
+let elemListMove = document.querySelectorAll(".list_move > div > a");
+let elemAnimateCss = document.querySelector(".animateCss");
+console.log(ArrayListMove.length);
+for (let i = 0; i < ArrayListMove.length; i++) {
+  elemListMove[i].onclick = () => {
+    elemTextView.innerHTML = `<h2 class="text-white"><<span class="text-yellow-600">h1</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">animate__animated  ${ArrayListMove[i]}</span>"> Animate.css <<span class="text-yellow-600">/h1</span>></h2> `;
+    handler(elemAnimateCss, ArrayListMove[i], 800);
+  };
+}
+
+//! -------------------------------------------------------------------------- */
+//* -------------------------- TheEndClassAnimation.css ----------------------------- */
+//! -------------------------------------------------------------------------- */
+// window.onscroll=()=>{
+//   console.log("test");
+// else{
+// }}
+
+//TODO ----------------------- FunctionViewClassAnimation ----------------------- */
+function handler(element, moveAnimate, time) {
+  element.classList.toggle(moveAnimate);
+  setTimeout(() => {
+    element.classList.remove(moveAnimate);
+  }, time);
+}
+//! -------------------------------------------------------------------------- */
+//* ----------------------------- StartAnimate.js ----------------------------- */
+//! -------------------------------------------------------------------------- */
+let elmAnimateJs = document.querySelector(".animate_js");
+let offsetAnimateJs = anime.setDashoffset(elmAnimateJs);
+elmAnimate.setAttribute("stroke-dashoffset", offsetAnimateJs);
+anime({
+  targets: elmAnimateJs,
+  strokeDashoffset: [offsetAnimateJs, 25],
+  duration: anime.random(3000, 2000),
+  delay: anime.random(1000, 0),
+  loop: true,
+  direction: "alternate",
+  easing: "easeInOutSine",
+  autoplay: true,
+});
+//TODO --------------- scrollAnimateJs ----------------- */
+// let body = document.body;
+let elmDivAnimateJs = document.querySelector(".divAnimateJs");
+let elmLineAnimate = document.querySelector("#lineAnimate");
+window.onscroll = () => {
+  winScroll(384, elmLineAnimate, "animate__backInRight"); //! <=== جواب
+  winScroll(845, elmDivAnimateJs, "animate__lightSpeedInLeft");
+};
+function winScroll(number, element, classId) {
+  if (window.scrollY > number) {
+    element.classList.add(classId);
+  } else {
+    element.classList.remove(classId);
+  }
+}
+
+//TODO --------------- TheEndScrollAnimateJs ----------------- */
+
+//! -------------------------------------------------------------------------- */
+//* ----------------------------- MenuAnimeJs -------------------------------- */
+//! -------------------------------------------------------------------------- */
+
+let elmMenuCodeAnimeJs = document.querySelector("#menuCodeAnimeJs");
+
+//TODO --------------- animeJs-1 ----------------- */
+let elemAnimeJs_1 = document.querySelector("#animejs-1");
+let elmTextAnimeJs = document.querySelector(".divMenuAnimeJs");
+let elmDivAnimeJs = document.querySelector("#divAll");
+console.log(elmDivAnimeJs);
+
+let createElm = document.createElement("div").innerHTML;
+console.log(elemAnimeJs_1);
+
+elemAnimeJs_1.addEventListener("click", animeJs_1, { once: true });
+console.log("best");
+let elmEl = document.querySelectorAll(".el");
+console.log(elmEl);
+
+function animeJs_1() {
+
+  createElm.classList =
+    "relative bottom-56 w-3/4 h-52 animate__animated animate__zoomInDown bg-slate-200/30 border-8 ring ring-rose-500 border-double";
+  elmDivAnimeJs.classList.remove("mt-52");
+  elmDivAnimeJs.classList.add("mt-2");
+
+//  elmTextAnimeJs.innerHTML="";
+ ////! source code  anime js 
+  elmTextAnimeJs.insertAdjacentHTML(
+    "beforeend",`<div class="div flex justify-center">
+  <div id="innerSourceCodeAnimeJs" class="relative flex p-2   w-3/4 h-42 z-10 animate__animated animate__zoomInDown bg-slate-200/30 border-8 ring ring-rose-500  border-double">
+   <div class="w-2/4 p-2 bg-rose-600/20 text-white h-auto">
+      <h2 class="text-white"><<span class="text-yellow-600">div</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">function-based-params-demo</span>"> 
+         <h2 class="text-white"><<span class="text-yellow-600">div</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">el</span>">   <<span class="text-yellow-600">/div</span>></h2>
+         <h2 class="text-white"><<span class="text-yellow-600">div</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">el</span>"> 
+            <<span class="text-yellow-600">/div</span>></h2>
+         <<span class="text-yellow-600">/div</span>></h2>
+   </div>
+   <div class="w-2/4 bg-rose-600/20 text-xs h-auto">
+      <pre><span class="text-blue-800">anime</span>({
+    <span class="text-green-600"> targets:</span> <span class="text-yellow-400">'.function-based-params-demo .el'</span>,
+    <span class="text-green-600">translateX:</span><span class="text-yellow-400"> 270,</span>
+    <span class="text-green-600">direction:</span> <span class="text-yellow-400">'alternate',</span>
+    <span class="text-green-600">loop:</span><span class="text-yellow-400"> true,</span>
+    <span class="text-blue-600">delay:</span> <span class="text-yellow-400">function(el, i, l) {</span>
+     <span class="text-green-600"></span><span class="text-purple-700"> return</span> i * 100;
+    <span class="text-yellow-400">},</span>
+    <span class="text-blue-600">endDelay:</span><span class="text-yellow-400">function(el, i, l) {</span>
+     <span class="text-purple-700"> return</span> (l - i) * 100;
+    <span class="text-blue-600">}</span> }); </pre>
+   </div>
+    </div>
+ </div>
+`
+    // textCodeMenuAnimeJs(codeAnimeJs1)
+  );
+ 
+  anime({
+    targets: ".function-based-params-anime-js .el",
+    translateX: 210,
+    direction: "alternate",
+    loop: true,
+    delay: function (el, i, l) {
+      return i * 500;
+    },
+    endDelay: function (el, i, l) {
+      return (l - i) * 100;
+    },
+  });
+}
+//  elemAnimeJs_1.onclick=()=>{
+//   console.log("test");
+//   let codeAnimeJs1=
+// textCodeMenuAnimeJs(codeAnimeJs1)
+
+// }
+// // console.log(codeAnimeJs1);
+// function textCodeMenuAnimeJs(codeAnimeJs){
+// return   codeAnimeJs
+// }
+//TODO --------------- animeJs-2 ----------------- */
+let elementAnimeJS_2=document.querySelector("#animejs-2");
+console.log(elementAnimeJS_2);
+elementAnimeJS_2.addEventListener('click',clickAnimejs_2,{ once: true })
+ function clickAnimejs_2() {
+  /*DEMO*/
+ let sourceCodeAnimJs = document.querySelector('#innerSourceCodeAnimeJs') 
+ console.log(sourceCodeAnimJs);
+
+  createElm.classList =
+    "relative bottom-56 w-3/4 h-52 animate__animated animate__zoomInDown bg-slate-200/30 border-8 ring ring-rose-500 border-double";
+  elmDivAnimeJs.classList.remove("mt-52");
+  elmDivAnimeJs.classList.add("mt-2");
+
+//  elmTextAnimeJs.innerHTML="";
+ ////! source code  anime js 
+  elmTextAnimeJs.insertAdjacentHTML(
+    "beforeend",`<div class="div flex justify-center">
+  <div id="innerSourceCodeAnimeJs" class="relative flex p-2   w-3/4 h-42 z-10 animate__animated animate__zoomInDown bg-slate-200/30 border-8 ring ring-rose-500  border-double">
+   <div class="w-2/4 p-2 bg-rose-600/20 text-white h-auto">
+      <h2 class="text-white"><<span class="text-yellow-600">div</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">function-based-params-demo</span>"> 
+         <h2 class="text-white"><<span class="text-yellow-600">div</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">el</span>">   <<span class="text-yellow-600">/div</span>></h2>
+         <h2 class="text-white"><<span class="text-yellow-600">div</span> <span class="text-yellow-400"> class=</span>"<span class="text-green-700">el</span>"> 
+            <<span class="text-yellow-600">/div</span>></h2>
+         <<span class="text-yellow-600">/div</span>></h2>
+   </div>
+   <div class="w-2/4 bg-rose-600/20 text-xs h-auto">
+      <pre><span class="text-blue-800">anime</span>({
+    <span class="text-green-600"> targets:</span> <span class="text-yellow-400">'.function-based-params-demo .el'</span>,
+    <span class="text-green-600">translateX:</span><span class="text-yellow-400"> 270,</span>
+    <span class="text-green-600">direction:</span> <span class="text-yellow-400">'alternate',</span>
+    <span class="text-green-600">loop:</span><span class="text-yellow-400"> true,</span>
+    <span class="text-blue-600">delay:</span> <span class="text-yellow-400">function(el, i, l) {</span>
+     <span class="text-green-600"></span><span class="text-purple-700"> return</span> i * 100;
+    <span class="text-yellow-400">},</span>
+    <span class="text-blue-600">endDelay:</span><span class="text-yellow-400">function(el, i, l) {</span>
+     <span class="text-purple-700"> return</span> (l - i) * 100;
+    <span class="text-blue-600">}</span> }); </pre>
+   </div>
+    </div>
+ </div>
+`)
+  // let elmInnerSourceCode = document.querySelector('#innerSourceCodeAnimeJs');
+  // elmInnerSourceCode.innerHTML="";
+  // console.log(elmInnerSourceCode);
+  // console.log("test");
+  anime({
+    targets: '.autoplay-true',
+    translateX: 200,
+    autoplay: true,
+    easing: 'easeInOutSine'
+  });
+  
+  anime({
+    targets: '.autoplay-true',
+    translateX: 200,
+    autoplay: false,
+    easing: 'easeInOutSine'
+  });
+  /*DEMO*/
+  animeJs_1()
+  }
+
+// function animeJs_2() {}
+
+menuAnimeJs();
+function menuAnimeJs(colorBorder, colorBg, colorShadow) {
+  let elmAll_Menu = document.querySelectorAll(".menuAnimeJs");
+  for (let i = 0; i < elmAll_Menu.length; i++) {}
+}
