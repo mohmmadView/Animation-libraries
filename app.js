7bc9d416a0a1b54d7fcb38b47c109d4263963aad
mohmmad.rahgoshahi <mamad.rahgoshahi@gmail.com>
2023-02-18 22:51:23 +0330

    mo.jsNew2

--- app.js
+++ app.js
@@ -582,7 +582,7 @@ console.log(ElmMoveJs);
     parent:       '#bouncyCircle',
     shape:        'circle',
     fill:         {'#F64040': '#FC46AD'},
-    radius:       {20: 40},
+    radius:       {20: 60},
     duration:     2000,
     isYoyo:       true,
     isShowStart:  true,
@@ -690,32 +690,33 @@ console.log(ElmMoveJs);
 
   
 
-  // const circle5 = new mojs.Shape({
-  //   parent:       '#circle5',
-  //   shape:        'circle',
-  //   scale:        { 0 : 1 },
-  //   left:         '25%',
-  //   fill:         { 'cyan': 'yellow' },
-  //   radius:       25,
+  const circle5 = new mojs.Shape({
+    parent:       '#circle5',
+    shape:        'circle',
+    scale:        { 0 : 1 },
+    left:         '25%',
+    fill:         { 'cyan': 'red' },
+    radius:       25,
   
-  //   duration:     2000,
-  //   repeat:       999,
-  // }).play();
+    duration:     2000,
+    repeat:       999,
+  }).play();
   
-  // const rect = new mojs.Shape({
-  //   shape:        'rect',
-  //   left:         '50%',
-  //   fill:         'none',
-  //   radius:       20,
-  //   stroke:       { 'rgba(0,255,255, 1)' : 'magenta' },
-  //   strokeWidth:  { 10: 0 },
-  //   strokeDasharray: '100%',
-  //   strokeDashoffset: { '-100%' : '100%' },
-  //   rotate:        { 0: 180 },
+  const rect3 = new mojs.Shape({
+    parent:        '#rect8',
+    shape:        'rect',
+    left:         '50%',
+    fill:         'none',
+    radius:       20,
+    stroke:       { 'rgba(0,255,255, 1)' : 'magenta' },
+    strokeWidth:  { 10: 0 },
+    strokeDasharray: '100%',
+    strokeDashoffset: { '-100%' : '100%' },
+    rotate:        { 0: 180 },
   
-  //   duration:     2000,
-  //   repeat:       999,
-  // }).play();
+    duration:     2000,
+    repeat:       999,
+  }).play();
   
   // const polygon = new mojs.Shape({
   //   shape:        'polygon',
