483ece9be96edde0c541108f2683c9346e1bd9b0
mohmmadView <mamad.rahgoshahi@gmail.com>
2024-02-13 03:00:36 +0330

    update change vite module update  GSAP

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ import Prism from 'prismjs';
 import AnimateCss from "./src/animateCss";
 import animateJS from "./src/animateJS";
 import animateMoJs from './src/MoJs'
+import animateGsap from './src/Gsap'
+import miniBusSvg from './src/svg/miniBusSvg';
+
+    
+
 AnimateCss()
 animateJS()
 animateMoJs()
+animateGsap()
+miniBusSvg()
